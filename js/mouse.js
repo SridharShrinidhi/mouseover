a46805f134fbfa67d6b70ef0bca50a46a57105fe
Shrinidhi Sridhar <srid0002@algonquinlive.com>
2020-04-07 22:20:05 -0400

    Change the event in js

--- js/mouse.js
+++ js/mouse.js
@@ -1,11 +1,12 @@
-$(document).on("mouseover", function (ev) {
+$(document).on("mousemove", function (ev) {
 
   var mouseX = ev.originalEvent.pageX;
   var mouseY = ev.originalEvent.pageY;
 
   $("img").each(function () {
-    var imgX = $(this).position().left + 70;
-    var imgY = $(this).position().top + 70;
+    console.log("calculating..");
+    var imgX = $(this).position().left + 200;
+    var imgY = $(this).position().top + 200;
 
     var diffX = mouseX - imgX;
     var diffY = mouseY - imgY;
@@ -15,14 +16,14 @@ $(document).on("mouseover", function (ev) {
     $(this).css("transform", "rotate(" + angle + "deg)");
   })
 });
-
-var count = 0;
-var images = ["images/brown-eye.svg", "images/green-eye.svg", "images/pink-eye.svg"];
-
-setInterval(function () {
-  count = count + 1;
-  count = count % images.length;
-  var image = images[count];
-  $("img").attr("src", image);
-
-}, 2000);
+//
+// var count = 0;
+// var images = ["images/brown-eye.svg", "images/green-eye.svg", "images/pink-eye.svg"];
+//
+// setInterval(function () {
+//   count = count + 1;
+//   count = count % images.length;
+//   var image = images[count];
+//   $("img").attr("src", image);
+//
+// }, 2000);
